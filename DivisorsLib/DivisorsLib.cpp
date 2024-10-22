347e06083d7338ee7387e37973afa8cb4b01e3d4
Divine-cel <divinsa1276_aehit@students.vizja.pl>
2024-10-22 12:28:47 +0200

    Updated divisorsLib.cpp

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,21 @@
+
 #include <vector>
+#include <cmath>
 #include "Divisors.h"
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+    std::vector<unsigned long long> result;
+    for (unsigned long long i = 1; i <= std::sqrt(number); ++i)
+    {
+        if (number % i == 0)
+        {
+            result.push_back(i); // i is a divisor
+            if (i != number / i) // Avoid adding the square root twice
+            {
+                result.push_back(number / i); // number / i is the corresponding divisor
+            }
+        }
+    }
+    return result;
 }
